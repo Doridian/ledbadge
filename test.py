bea9b05d54ab9b79fc2a9fbda75aa31fd2529b20
Mark Dietzer <mriq91@gmail.com>
2018-02-12 23:13:23 +0100

    docs

--- test.py
+++ test.py
@@ -1,12 +1,19 @@
-from converter import ImageMessage, convert, TextMessage
+from converter import ImageMessage, convert, TextMessage, Mode
+from bluetooth import send
 from PIL import Image
 
 img = Image.open('test.png')
-msg = ImageMessage(img)
-print(msg.data)
+# SNOWFLAKE is basically instant display. WARNING: All "pages" after 1 skip the first 4 rows.
+# So it displays like this [44 pixels] [4 pixels invisible] [44 pixels]
+# Make sure to take this into account when creating animations using SNOWFLAKE (see test.png provided)
+msg = ImageMessage(img, speed=0x3, mode=Mode.SNOWFLAKE)
 
-msg = TextMessage("12345123451234512")
-print(msg.data)
-print(msg.len)
+#msg = TextMessage('123451234512345', mode=Mode.SNOWFLAKE)
 
-#print(convert([TextMessage("Hello")]))
+data = convert([
+        msg
+])
+
+print(data)
+
+send("xx:xx:xx:xx:xx:xx", data)
